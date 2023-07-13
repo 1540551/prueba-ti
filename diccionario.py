9bea0c47cd765e6a13c6f410ecb4881d56b4e3f3
1540551 <138245839+1540551@users.noreply.github.com>
2023-07-13 12:44:46 -0500

    Add files via upload

---
+++ diccionario.py
@@ -0,0 +1,32 @@
+producto= {
+    "nombre":"",
+    "marca": "",
+    "costo":0,
+    "cantidad":0,
+}
+
+lista de productos []
+while glag:true 
+    try:
+        nombreporducto = input("nombrde del producto")
+        marcaproducto = input("marca del producto")
+        costoproducto = input("costo del prpoducto")
+        cantidadproducto = input("catidad del producto")
+    except ValueError 
+         print ("ALGO SALIO MAL INTENTALO OTRA VEZ")
+    else:
+        producto["nombre"]=nombreproduco
+        producto["marca"]=marcaproducto
+        producto["costo"]= costoproducto
+        producto["cantidad"]= cantidadproducto
+        lista_productos.append(producto)
+        producto ={}
+        pregunta = input("desea agregar mas productos")
+
+
+        supermercado 
+        elija la opcion 
+1.listar los productos 
+2.agregar mas productos 
+3.hacer la venta 
+    
