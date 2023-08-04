e951389e9ec47f7684139af2f51f59e50d5f45f1
1540551 <138245839+1540551@users.noreply.github.com>
2023-08-04 11:18:47 -0500

    Update diccionario.py

--- diccionario.py
+++ diccionario.py
@@ -1,32 +1,41 @@
-producto= {
-    "nombre":"",
+producto = {
+    "nombre": "", 
     "marca": "",
-    "costo":0,
-    "cantidad":0,
+    "costo": 0,
+    "cantidad": 0,
 }
 
-lista de productos []
-while glag:true 
-    try:
-        nombreporducto = input("nombrde del producto")
-        marcaproducto = input("marca del producto")
-        costoproducto = input("costo del prpoducto")
-        cantidadproducto = input("catidad del producto")
-    except ValueError 
-         print ("ALGO SALIO MAL INTENTALO OTRA VEZ")
+lista_productos = []
+
+flag=True
+while flag:
+    try: 
+        nombreproducto = input("Nombre del producto: ")
+        marcaproducto = input("Marca del producto: ")
+        costoproducto = int(input("Costo del producto: "))
+        cantidadproducto = int(input("Cantidad del producto: "))
+    except ValueError:
+        print("ALGO SALIO MAL INTENTALO OTRA VEZ")
     else:
-        producto["nombre"]=nombreproduco
-        producto["marca"]=marcaproducto
+        producto["nombre"]= nombreproducto
+        producto["marca"]= marcaproducto
         producto["costo"]= costoproducto
         producto["cantidad"]= cantidadproducto
         lista_productos.append(producto)
-        producto ={}
-        pregunta = input("desea agregar mas productos")
+        producto = {}
+        pregunta = input("Desea agrear mas Productos? SI/NO")
+        if str(pregunta) != "SI":
+            flag = False
 
+        print(lista_productos)
 
-        supermercado 
-        elija la opcion 
-1.listar los productos 
-2.agregar mas productos 
-3.hacer la venta 
-    
+#menu
+print("""
+      Supermercado 
+      elija la opcion
+1. listar los productos
+2. agregar mas productos
+3. hacer la venta.
+""")
+
+opcion = input("elija la opcion del menu: ")
